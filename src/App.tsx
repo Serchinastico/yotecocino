135e7aed41300cac1cbb3e08f11ac1d30dc3738a
Serchinastico <54cymru@gmail.com>
2020-03-23 09:51:10 +0100

    Use create-react-app to create an empty app with typescript, redux and so

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+};
+
+export default App;
