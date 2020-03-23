dd13571e5f011b052292bb90a754562bd50f009b
Serchinastico <54cymru@gmail.com>
2020-03-23 10:43:03 +0100

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import WelcomeScreen from "feature/welcome/WelcomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <header className="App-header"></header>
-      <WelcomeScreen />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <WelcomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
