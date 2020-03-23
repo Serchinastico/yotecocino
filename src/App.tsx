1b148e701fb7f33b3fb11ca0da9ccad79fd60095
Serchinastico <54cymru@gmail.com>
2020-03-23 10:36:55 +0100

    Show the welcome page where users decide if they want to give food or receive it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import WelcomeScreen from "feature/welcome/WelcomeScreen";
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
       <header className="App-header"></header>
+      <WelcomeScreen />
     </div>
   );
 };
---
+++ src/feature/welcome/WelcomeScreen.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+
+const AcceptOrProvideFoodContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #f0f0f0;
+  border-radius: 16px;
+  box-shadow: 0px 4px 2px rgba(0, 0, 0, 0.2);
+  height: 90vh;
+  width: 45vw;
+  margin: 16px;
+  padding: 16px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AcceptOrProvideFoodTitle = styled.p`
+  font-size: 2rem;
+  line-height: 1.5;
+  width: 50%;
+`;
+
+const Tag = styled.span`
+  background: #c07ee280;
+  border: 1px solid #c07ee2ff;
+  padding: 4px;
+  border-radius: 4px;
+`;
+
+const AcceptOrProvideFoodIllustration = styled.img`
+  width: 50%;
+  height: 50%;
+  justify-self: flex-end;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const WelcomeScreen: React.FC = () => {
+  const AcceptFoodOption = () => {
+    return (
+      <AcceptOrProvideFoodContainer>
+        <AcceptOrProvideFoodTitle>
+          Soy <Tag>personal sanitario</Tag>,{" "}
+          <Tag>miembro de los cuerpos policiales</Tag>,{" "}
+          <Tag>cajer@ en un supermercado</Tag>, o en definitiva, cualquier
+          persona que esté trabajando durante estos días.
+        </AcceptOrProvideFoodTitle>
+        <AcceptOrProvideFoodIllustration src={"img/il_doctor.svg"} />
+      </AcceptOrProvideFoodContainer>
+    );
+  };
+
+  const ProvideFoodOption = () => {
+    return (
+      <AcceptOrProvideFoodContainer>
+        <AcceptOrProvideFoodTitle>
+          Me ofrezco a hacer comida para l@s trabajadores.
+        </AcceptOrProvideFoodTitle>
+        <AcceptOrProvideFoodIllustration src={"img/il_chef.svg"} />
+      </AcceptOrProvideFoodContainer>
+    );
+  };
+
+  return (
+    <Container>
+      <AcceptFoodOption />
+      <ProvideFoodOption />
+    </Container>
+  );
+};
+
+export default WelcomeScreen;
