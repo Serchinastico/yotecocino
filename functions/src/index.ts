c2ee9c5d6da4d8399b2543d8ad664a8610a7c505
Serchinastico <54cymru@gmail.com>
2020-03-24 17:12:36 +0100

    Fix compilation issues and deploy function on europe-west1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ import * as functions from "firebase-functions";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+export const helloWorld = functions
+  .region("europe-west1")
+  .https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+  });
