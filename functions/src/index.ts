5edc45655a7d0eade3c978beb01bce7476cbbe82
Serchinastico <54cymru@gmail.com>
2020-03-24 18:34:13 +0100

    Create first function that retrieves information from firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,37 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+interface FoodOffer {
+  contact: string;
+  day: Date;
+  food_name: string;
+  location: Location;
+  location_geohash: string;
+  service: string;
+}
 
 export const createFoodOffer = functions
   .region("europe-west1")
-  .https.onRequest((request, response) => {
+  .https.onRequest(async (request, response) => {
+    admin.initializeApp();
+    const document = await admin
+      .firestore()
+      .collection("food-offers")
+      .where("contact", "==", "@serchinastico")
+      .get();
+
+    const result: FoodOffer = document.docs[0].data() as FoodOffer;
+    response.send(`Holi: ${JSON.stringify(result)}`);
     response.sendStatus(201);
   });
 
 export const deleteFoodOffer = functions
+  .region("europe-west1")
+  .https.onRequest((request, response) => {
+    response.sendStatus(200);
+  });
+
+export const searchFoodOffers = functions
   .region("europe-west1")
   .https.onRequest((request, response) => {
     response.sendStatus(200);
