6e4302a08f16f50b6dbd1ff206f14f7eed5fdc75
Serchinastico <54cymru@gmail.com>
2020-03-25 11:23:07 +0100

    Add privacy policy to the footer

--- src/feature/ui/Footer.tsx
+++ src/feature/ui/Footer.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { CreatorsDiv, Creator, Creators } from "./StyledFooter";
+import { FooterContainer, FooterLink, FooterContent } from "./StyledFooter";
 
 const Footer: React.FC = () => {
   return (
-    <CreatorsDiv>
-      <Creators>
-        Creado con mucho{" "}
-        <span role="img" aria-label="amor">
-          ❤️
-        </span>{" "}
-        por{" "}
-        <Creator href="https://twitter.com/Serchinastico">
-          @Serchinastico
-        </Creator>
-        , <Creator href="https://twitter.com/delr3ves">@delr3ves</Creator> y{" "}
-        <Creator href="https://www.linkedin.com/in/aliciacarbajalzapater/">
-          @alicarbajal
-        </Creator>
-      </Creators>
-    </CreatorsDiv>
+    <FooterContainer>
+      <FooterContent>
+        Lee nuestra{" "}
+        <FooterLink href="privacypolicy.html">
+          política de privacidad
+        </FooterLink>
+      </FooterContent>
+    </FooterContainer>
   );
 };
 
